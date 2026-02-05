15e89778bdd0bbced0abc70d938b1a4fc6e52627
Alesseg <alejandro.seguido@estudiante.uam.es>
2026-02-05 17:59:56 +0100

    nothing

--- Biblioteca.java
+++ Biblioteca.java
@@ -4,29 +4,23 @@ public class Biblioteca {
     private String nombre;
     private List<Libro> libros;
 
-    // 2. Las estructuras EFICIENTES (Índices)
-    private Map<String, List<Libro>> porGenero; // HashMap (rápido)
-    private TreeMap<Integer, List<Libro>> porAno; // TreeMap (ordenado)
+    // Las estructuras EFICIENTES (Índices)
+    private HashMap<String, List<Libro>> porGenero; // HashMap (rápido)
+    private TreeMap<Integer, List<Libro>> porAnno; // TreeMap (ordenado)
 
-    // Constructor
-    public Biblioteca(String nombre, List<Libro> libros) {
+    /** 
+     * Instancia los atributos nombre y la lista de libros. Introduce los libros a los índices correspondientes. 
+    */
+    public Biblioteca(String nombre, List<Libro> libros){
         this.nombre = nombre;
-        if (libros == null) {
+        //this.libros = libros;
+        if(libros == null){
             this.libros = new ArrayList<>();
         } else {
-            this.libros = libros; // haciendo esto si modifican la lista original tambien se modifica la de la
-                                  // biblioteca
+            this.libros = libros; //haciendo esto, si modifica la lista original tambien se modifica la de la biblioteca
         }
-        this.porGenero = new HashMap<>(); // inicializamos el mapa
-        this.porAno = new TreeMap<>(); // inicializamos el treemap
-
-        if (libros != null) {
-            for (Libro l : libros) {
-                this.nuevoLibro(l); // añadimos los libros a la biblioteca, se podría llegar a quitar el this ya que
-                                    // se utiliza dentro de la misma clase
-            }
-        }
-
+        this.porGenero = new HashMap<>(); //inicializamos el mapa
+        this.porAnno = new TreeMap<>(); //inicializamos el treemap        
     }
 
     public Biblioteca(String nombre) {
