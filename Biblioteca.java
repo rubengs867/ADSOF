7a6edba6d95e40a2b9089ba844098a868ea05e09
Alesseg <alejandro.seguido@estudiante.uam.es>
2026-02-05 23:25:20 +0100

    librosPosterioresA terminado. añadirLibro modificado. Libro actualizado.

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,53 +1,75 @@
+
 import java.util.*;
 
 public class Biblioteca {
-    private String nombre;
-    private List<Libro> libros;
 
-    // Las estructuras EFICIENTES (Índices)
-    private HashMap<String, List<Libro>> porGenero; // HashMap (rápido)
-    private TreeMap<Integer, List<Libro>> porAnno; // TreeMap (ordenado)
+  private String nombre;
+  private List<Libro> libros;
 
-    /** 
-     * Instancia los atributos nombre y la lista de libros. Introduce los libros a los índices correspondientes. 
-    */
-    public Biblioteca(String nombre, List<Libro> libros){
-        this.nombre = nombre;
-        //this.libros = libros;
-        if(libros == null){
-            this.libros = new ArrayList<>();
-        } else {
-            this.libros = libros; //haciendo esto, si modifica la lista original tambien se modifica la de la biblioteca
-        }
-        this.porGenero = new HashMap<>(); //inicializamos el mapa
-        this.porAnno = new TreeMap<>(); //inicializamos el treemap        
+  private HashMap<String, List<Libro>> porGenero; // HashMap (rápido)
+  private TreeMap<Integer, List<Libro>> porAño; // TreeMap (ordenado)
+
+  /**
+   * Instancia los atributos nombre y la lista de libros. Introduce los libros
+   * a los índices correspondientes.
+   */
+  public Biblioteca(String nombre, List<Libro> libros) {
+    this.nombre = nombre;
+    if (libros == null) {
+      this.libros = new ArrayList<>();
+    } else {
+      this.libros = libros; //haciendo esto, si modifica la lista original tambien se modifica la de la biblioteca
     }
+    this.porGenero = new HashMap<>(); // inicializamos el mapa
+    this.porAño = new TreeMap<>(); // inicializamos el treemap
+  }
 
-    public Biblioteca(String nombre) {
-        this.nombre = nombre;
-        this.porGenero = new HashMap<>(); // inicializamos el mapa
-        this.porAno = new TreeMap<>(); // inicializamos el treemap
-        this.libros = new ArrayList<>(); // inicializamos la lista de libros
-    }
+  public Biblioteca(String nombre) {
+    this.nombre = nombre;
+    this.porGenero = new HashMap<>(); // inicializamos el mapa
+    this.porAño = new TreeMap<>(); // inicializamos el treemap
+    this.libros = new ArrayList<>(); // inicializamos la lista de libros
+  }
 
-    public void nuevoLibro(Libro libro) {
+  public void añadirLibro(Libro libro) {
+    String genero = libro.getGenero();
+    int año = libro.getAño();
+    
+    this.libros.add(libro); // Añadimos el libro a la lista
+    
+    /* Añadimos el libro al Hashmap en función del género */
+    // this.porGenero.putIfAbsent(genero, new ArrayList<>()); // Crea una lista vacía en caso de no existir el género
+    // this.porGenero.get(genero).add(libro); // Anade el libro
+    this.porGenero.computeIfAbsent(genero, k -> new ArrayList<>()).add(libro);
 
-        String genero = libro.getGenero();
-        this.libros.add(libro); // añadimos a la lista de libros
-        // if(!this.porGenero.containsKey(genero)){
-        // this.porGenero.put(genero, new ArrayList<>()); //si no existe el genero lo
-        // creamos
-        // }
-        // this.porGenero.get(genero).add(libro); //añadimos el libro al genero
-        // correspondiente
-        this.porGenero.computeIfAbsent(genero, k -> new ArrayList<>()).add(libro); // esto hace lo mismo que las 3
-                                                                                   // lineas comentadas arriba
+    /* Añadimos el libro al Treemap en función del año */
+    // this.porAño.putIfAbsent(año, new ArrayList<>()); // Crea una lista vacía en caso de no existir el año
+    // this.porAño.get(año).add(libro); // Añade el libro
+    this.porAño.computeIfAbsent(año, k -> new ArrayList<>()).add(libro);
+  }
 
-    }
+  /**
+   * Devuelve una lista con todos los libros que pertenecen a un determinado género. 
+   * En caso de no existir el género, devuelve una lista vacía.
+   */
+  public List<Libro> librosPorGenero(String genero) {
+    return this.porGenero.getOrDefault(genero, new ArrayList<>()); // si no existe el genero devuelve una lista vacia
+  }
 
-    public List<Libro> buscarPorGenero(String genero) {
-        return this.porGenero.getOrDefault(genero, new ArrayList<>()); // si no existe el genero devuelve una lista
-                                                                       // vacia
-    }
+  /**
+   * Devuelve una lista con todos los libros con todos los libros publicados después de ese año.
+   * En caso de no haber, devuelve una lista vacía
+   */
+  public List<Libro> librosPosterioresA(int añoPublicacion) {
+    List<Libro> total = new ArrayList<>();
+    
+    // Guarda las listas de libros cuyo año de publicación es mayor al dato pasado
+    NavigableMap<Integer, List<Libro>> librosFiltrados = this.porAño.tailMap(añoPublicacion, false);
 
+    // Junto todos los libros en la misma lista
+    for (List<Libro> l : librosFiltrados.values())
+      total.addAll(l);
+
+    return total;
+  }
 }
--- BibliotecaApp.java
+++ BibliotecaApp.java
@@ -1,19 +1,29 @@
 import java.util.*;
 public class BibliotecaApp {
-    public static void main (String[] args){
-        List<Libro> libros = new ArrayList<>( List.of(
-            new Libro("1", "El Quijote", "Miguel de Cervantes", 5),
-            new Libro("2", "El múrcielago", "Jo Nesbo", 1),
-            new Libro ("3", "Learn Java", "David Hoffman", 6)));
-        
-        libros.get(1).prestar();
-        for(Libro l : libros){
-            System.out.println(l);
-        }
-        libros.get(1).devolver();
-        System.out.println(libros);
-
-        libros.add(new Libro("4", "Con viento solano", "Ignacio Aldecoa", 1));
-        System.out.println(libros);
+  public static void main (String[] args){
+    List<Libro> libros = new ArrayList<>( List.of(
+      new Libro("1", "El Quijote", "Miguel de Cervantes", 5, "Ficcion", 1605),
+      new Libro("2", "El múrcielago", "Jo Nesbo", 2, "Ficcion", 1900),
+      new Libro ("3", "Learn Java", "David Hoffman", 6, "Ficcon", 2000)));
+    libros.get(1).prestar();
+    for(Libro l : libros){
+      System.out.println(l);
     }
+    libros.get(1).devolver();
+    System.out.println(libros);
+
+    libros.add(new Libro("4", "Con viento solano", "Ignacio Aldecoa", 1));
+    System.out.println(libros);
+    
+    // Creo una biblioteca
+    Biblioteca biblio = new Biblioteca("alejandro");
+    // Añado a la biblioteca todos los libros
+    for(Libro l : libros)
+      biblio.añadirLibro(l);
+
+    // Imprimo los libros con género X
+    System.out.println("\n\n" + biblio.librosPorGenero("Ficcion") + "\n");
+    // Imprimo los libros posteriores a X año
+    System.out.println(biblio.librosPosterioresA(1800) + "\n");
+  }
 }
