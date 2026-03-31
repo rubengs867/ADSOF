15e974ad2f42d8ac9a63a19b82ee51cf2e5fa9cd
rubengs867 <ruben.garsan06@gmail.com>
2026-03-31 16:14:55 +0200

    nuevaclase

--- Practica4/Procesador/ProcesadorDatos.java
+++ Practica4/Procesador/ProcesadorDatos.java
@@ -4,14 +4,43 @@ import Practica4.Conversor.*;
 import Practica4.sensor.Unidad;
 import java.time.LocalDateTime;
 import java.util.*;
+
 public class ProcesadorDatos {
-  
+
   private Conversor conversor;
 
-  private Map <LocalDateTime, Double> historial;
+  private Map<LocalDateTime, Double> historial;
 
-  public ProcesadorDatos(Unidad unidadOrigen){
+  public ProcesadorDatos(Unidad unidadOrigen) {
     this.conversor = new ConversorIdentidad(unidadOrigen);
-    this.historial = new HashMap<>();
+    //usamos treemap para ordenar por fechas
+    this.historial = new TreeMap<>();
+  }
+
+  public void setConversor(Conversor conversor) {
+    this.conversor = conversor;
+  }
+
+  /**
+   * Convierte el dato y lo guarda en el mapa usando la fecha como clave.
+   */
+  public void procesarLectura(LocalDateTime fechaHora, double valorLeido) {
+    double valorConvertido = this.conversor.convertir(valorLeido);
+    this.historial.put(fechaHora, valorConvertido);
+  }
+
+  public double getMinimo(){
+    if(this.historial.isEmpty()) return 0.0;
+    return this.historial.values().stream().mapToDouble(u -> u).min().getAsDouble();
+  }
+
+  public double getMaximo(){
+    if(this.historial.isEmpty()) return 0.0;
+    return this.historial.values().stream().mapToDouble(u -> u).max().getAsDouble();
+  }
+
+  public double getMedia(){
+    if(this.historial.isEmpty()) return 0.0;
+    return this.historial.values().stream().mapToDouble(u -> u).average().getAsDouble();
   }
 }
