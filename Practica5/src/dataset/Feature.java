5477332346708ffda543bd3a040d3677613f50ab
rubengs867 <ruben.garsan06@gmail.com>
2026-04-13 18:30:49 +0200

    feature

--- Practica5/src/dataset/Feature.java
+++ Practica5/src/dataset/Feature.java
@@ -1,7 +1,44 @@
 package dataset;
 
 import java.util.ArrayList;
+import java.util.*;
 
 public class Feature<T extends Comparable<T>> extends ArrayList<T> {
+
+  public T min() {
+    if (this.isEmpty())
+      return null;
+    T min = this.get(0);
+    for (T element : this) {
+      if (element.compareTo(min) < 0)
+        min = element;
+    }
+    return min;
+  }
+
+  public T max(){
+    if(this.isEmpty()){
+      return null;
+    }
+    T max = this.get(0);
+    for(T element : this){
+      if(element.compareTo(max)> 0){
+        max = element;
+      }
+    }
+    return max;
+  }
+
+  public Map<T, Integer> distribution(){
+    if(this.isEmpty()){
+      return null;
+    }
+    Map<T, Integer> frecuencia= new HashMap<>();
+    for(T element : this){
+      frecuencia.compute(element, (k,v) -> (v == null) ? 1 : v + 1);
+    }
+    return frecuencia;
+  }
+
 
 }
