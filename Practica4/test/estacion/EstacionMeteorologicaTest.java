fbf946f2d122df5a02326c36f9cc4187dffd4ca2
Alesseg <alejandro.seguido@estudiante.uam.es>
2026-04-13 10:01:43 +0200

    sobraba un import

--- Practica4/test/estacion/EstacionMeteorologicaTest.java
+++ Practica4/test/estacion/EstacionMeteorologicaTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import java.lang.reflect.Field;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
