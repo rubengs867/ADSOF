9fe11a359d1a178e7ab364e8b7f107b060eb3cc0
rubengs867 <ruben.garsan06@gmail.com>
2026-04-16 11:07:21 +0200

    main de prueba

---
+++ Practica5/test/apartado1/Main.java
@@ -0,0 +1,34 @@
+package apartado1;
+
+import dataset.*;
+import java.util.Collections; 
+
+public class Main { 
+  public static void main(String[] args) {
+    Dataset<Person> dataSet = buildDataSet();
+    System.out.println("dataset: " + dataSet);
+
+    dataSet.removeDuplicates();
+    System.out.println("dataset w/o duplicates: " + dataSet);
+
+    Feature<Integer> ages = dataSet.feature("age");
+    System.out.println("Ages: " + ages);
+    Collections.sort(ages);
+    System.out.println("Ages sorted: " + ages);
+    System.out.println("Min age: " + ages.min());
+    System.out.println("Gender distribution: " + dataSet.feature("gender").distribution()); // freq. of each value
+  }
+
+  public static Dataset<Person> buildDataSet() {
+    Person people[] = { new Person("Pedro", 66, 75, 180, true), // name, age, weight, height, male?
+        new Person("Ana", 47, 54, 158, false),
+        new Person("Luis", 34, 75, 176, true),
+        new Person("Rosa", 47, 54, 158, false)
+    };
+
+    Dataset<Person> dataSet = new Dataset<>(new PersonFeaturizer()); // A Featurizer for Person objects
+    dataSet.addAll(people);
+    return dataSet;
+  }
+
+} 
